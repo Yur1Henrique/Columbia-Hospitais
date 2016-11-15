dabb15637e47dc9830e4111b34e2a3b9f87b3cd8
Pedro Seabra <pedroseabra98@hotmail.com>
2016-11-15 21:10:52 -0200

    Commit Inicial
    Commit inicial para subir os arquivos ao repositório

---
+++ ColumbiaHospitais/Usuario/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <!-- Icone que aparece no navegador -->
+        <link rel="shortcut icon" href="icone.png" type="image/x-png">
+        <!-- Chamando o css -->
+        <link rel="stylesheet" type="text/css" href="css/home.css">
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
